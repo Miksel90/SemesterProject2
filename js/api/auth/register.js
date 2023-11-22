3fdaac05956cfb17d2dbec7739a4de0e679844b4
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-11-22 10:40:37 +0100

    working on login code

---
+++ js/api/auth/register.js
@@ -0,0 +1,45 @@
+import {
+  API_BASE_URL,
+  authURL,
+  register_endpoint,
+} from "../../consts/consts.mjs";
+
+async function registerUser(url, userData) {
+  try {
+    const postData = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    };
+    const response = await fetch(url, postData);
+    console.log(response);
+    const json = await response.json();
+    if (response.ok) {
+      window.location.href = "index.html";
+    }
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const userName = document.getElementById("userName").value;
+    const userEmail = document.getElementById("userEmail").value;
+    const userPassword = document.getElementById("userPassword").value;
+
+    const userData = {
+      name: userName,
+      email: userEmail,
+      password: userPassword,
+    };
+
+    const registerUrl = `${API_BASE_URL}${authURL}${register_endpoint}`;
+    await registerUser(registerUrl, userData);
+  });
---
+++ js/consts/consts.mjs
@@ -0,0 +1,7 @@
+export const API_BASE_URL = "https://api.noroff.dev/api/v1/";
+export const authURL = "auction/auth/";
+export const register_endpoint = "register";
+export const loginURL = "login";
+export const token = localStorage.getItem("accessToken");
+export const userName = localStorage.getItem("userName");
+export const userEmail = localStorage.getItem("userEmail");
