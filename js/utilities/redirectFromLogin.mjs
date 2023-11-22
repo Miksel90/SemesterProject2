34fa55b56e1b37519fac16553d0f81a655f2f785
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-11-22 22:21:43 +0100

    working on profile js, building html dynamically

--- js/utilities/redirectFromLogin.mjs
+++ js/utilities/redirectFromLogin.mjs
@@ -1,7 +1,5 @@
-import { userName, token } from "../consts/consts.mjs";
-
-export function redirectToProfile(userName, token, isLogout = false) {
-  if (userName && token && !isLogout) {
-    window.location.href = "profile/index.html";
-  }
-}
+// export function redirectToProfile(userName, token, isLogout = false) {
+//   if (userName && token && !isLogout) {
+//     window.location.href = "profile/index.html";
+//   }
+// }
--- js/utilities/utilities.mjs
+++ js/utilities/utilities.mjs
@@ -1,4 +1,15 @@
 import { setupScrollBehavior } from "./backToTop.mjs";
-import { redirectToProfile } from "./redirectFromLogin.mjs";
-redirectToProfile();
+// import { redirectToProfile } from "./redirectFromLogin.mjs";
+// redirectToProfile();
 setupScrollBehavior();
+
+const accessToken = localStorage.getItem("accessToken");
+const redirectLink = document.getElementById("redirectLink");
+
+redirectLink.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if (accessToken) {
+    window.location.href = "/profile/index.html";
+  }
+});
