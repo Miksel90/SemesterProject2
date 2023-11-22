919a15a1026b1de7b79459467e159a4c6c633e82
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-11-22 17:58:11 +0100

    working on profile js, building html dynamically

--- js/consts/consts.mjs
+++ js/consts/consts.mjs
@@ -5,3 +5,4 @@ export const register_endpoint = "register";
 export const login_endpoint = "login";
 export const userName = localStorage.getItem("userName");
 export const userEmail = localStorage.getItem("userEmail");
+export const token = localStorage.getItem("accessToken");
